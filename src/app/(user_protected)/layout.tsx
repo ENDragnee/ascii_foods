3ddbc7dad7481263c5210e9ac29244b817a3ef22
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2025-11-15 04:15:11 +0300

    update: order page for user

--- src/app/(user_protected)/layout.tsx
+++ src/app/(user_protected)/layout.tsx
@@ -14,14 +14,8 @@ export default async function UserDashboardLayout({
   }
 
   return (
-    <div className="min-h-screen w-full bg-muted/40">
-      <div className="flex flex-col sm:pl-14 md:pl-64">
-        <main className="flex-1 p-4 sm:p-6">
-          <AblyProvider>
-            {children}
-          </AblyProvider>
-        </main>
-      </div>
-    </div>
+    <AblyProvider>
+      {children}
+    </AblyProvider>
   );
 }
